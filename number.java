0c5fd128d279e89026e19c2b180112485c3c914c
Pradeepthi127 <160829907+Pradeepthi127@users.noreply.github.com>
2024-04-04 11:27:28 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,10 @@
+class number{
+       public static void main(String args[]){
+       System.out.println("print first 10 natural numbers:");
+       for(int i=1;i<=10;i++){
+          System.out.println(i+" ");
+          }
+       }
+}
+   
+
