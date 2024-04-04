77a2863349ceec60c319a4561fb0a646f605059e
Pradeepthi127 <160829907+Pradeepthi127@users.noreply.github.com>
2024-04-04 11:27:56 +0530

    Add files via upload

---
+++ Typeconv.java
@@ -0,0 +1,9 @@
+public class TypeConv {
+       public static void main(Strng[]args){
+           int rno=81;
+           double rnn=rno;
+           System.out.println(rno);
+           System.out.println(rnn);
+        }
+}
+          
